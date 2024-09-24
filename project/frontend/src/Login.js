c89de648fc8af61ef93ea06dfab6e7cd21b63ce2
humbertovenavente <jnajar@unis.edu.gt>
2024-09-23 19:33:17 -0600

    This is an update regarding validation of Sign in and Login, so if empty, will give an error, if do not match, will appear did not match with a user.

--- project/frontend/src/Login.js
+++ project/frontend/src/Login.js
@@ -1,38 +1,62 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Validation from './loginValidation';
 
 function Login() {
-  const navigate = useNavigate(); // Hook para navegar a otra página
 
-  // Lógica de acceso como invitado
+  const navigate = useNavigate(); 
+
+  // acceso como invitado
   const handleGuestAccess = () => {
-    navigate('/home'); // Redirige a la página de inicio (homepage)
+    navigate('/home'); 
+  };
+
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const validationErrors = Validation(values);
+    setErrors(validationErrors);
+
+  };
+
+  const handleInput =(event) =>{
+    setValues(prev => ({...prev, [event.target.name]: event.target.value}));
+
   };
 
   return (
     <div className='d-flex justify-content-center align-items-center vh-100' style={{ backgroundColor: 'darkorange' }}> 
       <div className='bg-white p-3 rounded w-25'>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <p>Log In</p>
        
           <div className='mb-3'> 
             <label htmlFor='email'>Email</label>
-            <input type="email" placeholder='Enter email' className='form-control rounded-0'></input>
+            <input type="email" placeholder='Enter email' name='email' onChange={handleInput} className='form-control rounded-0'></input>
+         {errors.email && <span className='text-danger'>{errors.email}</span>}
           </div>
 
           <div className='mb-3'>
             <label htmlFor='password'>Password</label>
-            <input type="password" placeholder='Enter password' className='form-control rounded-0'></input>
+            <input type="password" placeholder='Enter password' name='password' onChange={handleInput} className='form-control rounded-0'></input>
+            {errors.password && <span className='text-danger'>{errors.password}</span>}
           </div>
 
-          {/* Enlace de Forgot Password alineado a la derecha */}
+          
           <div className="text-end mb-3">
             <Link to="/forgotpassword" className="text-decoration-none">Forgot password?</Link>
           </div>
 
-          {/* Agrupar botones verticalmente */}
+         
           <div className='d-grid gap-2'>
-            <button className='btn btn-success' onClick={handleGuestAccess} >Log in</button>
+            <button type= 'submit' className='btn btn-success'> Log in</button>
 
             <Link to="/signup" className='btn btn-default border bg-light rounded-0 text-decoration-none'>Create an Account</Link>
             <p></p>
@@ -43,9 +67,13 @@ function Login() {
             >
               Access as Guest
             </button>
+         
           </div>
+       
         </form>
+     
       </div>
+   
     </div>
   );
 }
--- project/frontend/src/Signup.js
+++ project/frontend/src/Signup.js
@@ -1,23 +1,74 @@
-import React from 'react'
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom'
+import Validation from './signupValidation';
 
 function Signup() {
+
+  const [values, setValues] = useState({
+    name: '',
+    lastname: '',
+    email: '',
+    password: '',
+
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const validationErrors = Validation(values);
+    setErrors(validationErrors);
+
+  };
+
+  const handleInput =(event) =>{
+    setValues(prev => ({...prev, [event.target.name]: event.target.value}));
+
+  };
+
   return (
     <div className='d-flex justify-content-center align-items-center vh-100' style={{ backgroundColor: 'darkorange'}}> 
     <div className='bg-white p-3 rounded w-25'>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
       <p>Sign up</p>
+
+      <div className='mb-3'>
+          <label htmlFor='name'> Name </label>
+          <input type="name"placeholder='Enter name' name='name' onChange={handleInput} className='form-control rounded-0'></input>
+          {errors.name && <span className='text-danger'>{errors.name}</span>}
+        </div>
+
+        <div className='mb-3'>
+          <label htmlFor='lastname'> Lastname </label>
+          <input type="lastname"placeholder='Enter lastname' lastname='lastname' onChange={handleInput} className='form-control rounded-0'></input>
+          {errors.lastname && <span className='text-danger'>{errors.lastname}</span>}
+        </div>
 
         <div className='mb-3'>
           <label htmlFor='email'> Email </label>
-          <input type="email"placeholder='Enter email' className='form-control rounded-0'></input>
+          <input type="email"placeholder='Enter email' email='email' onChange={handleInput} className='form-control rounded-0'></input>
+          {errors.email && <span className='text-danger'>{errors.email}</span>}
         </div>
 
         <div className='mb-3'>
           <label htmlFor='password'> Password </label>
-          <input type="password" placeholder='Enter password' className='form-control rounded-0'></input>
+          <input type="password" placeholder='Enter password' password='password' onChange={handleInput} className='form-control rounded-0'></input>
+          {errors.password && <span className='text-danger'>{errors.password}</span>}
         </div>
-        <button className='btn btn-success w-100' > Create an account</button>
+    
+        <div className='mb-3'>
+          <label htmlFor='role'> Select role</label>
+          <select className='form-select' id='role'> {/* Form-select vi que era una clase de BS para darle estilo */}
+              <option value="user">User</option>
+              <option value="employee">Employee</option>
+              <option value="admin">Admin</option>
+            </select>
+
+        </div>
+
+
+
+        <button type='submit' className='btn btn-success w-100' > Create an account</button>
       <p></p>
       <Link to="/" className='btn btn-default border w-100 gb-light rounded-0 text-decoration-none'>Back</Link>
       
